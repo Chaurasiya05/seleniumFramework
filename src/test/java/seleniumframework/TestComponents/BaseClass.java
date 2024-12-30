ef08c0a3d95568d760ab16532689e37bfe80c45d
anubhav <anubhavchaurasiya22@gmail.com>
2024-12-30 19:59:18 +0530

    Anubhav commit

--- src/test/java/seleniumframework/TestComponents/BaseClass.java
+++ src/test/java/seleniumframework/TestComponents/BaseClass.java
@@ -34,14 +34,16 @@ public class BaseClass {
 	public LandingPage landingpage;
 	public Properties prop;
 	public String url;
+
 	public WebDriver initDriver() throws IOException {
 		prop = new Properties();
 //		FileInputStream path = new FileInputStream(System.getProperty("user.dir")
 //				+ "/src\\main\\java\\seleniumframework\\Resource\\GlobalData.properties");
 //		prop.load(path);
-	    prop=getPropertyObject(prop);
-	    String browserName = System.getProperty("browser")!=null?System.getProperty("browser"):prop.getProperty("browser");
-		 browserName = prop.getProperty("browser");
+		prop = getPropertyObject(prop);
+		String browserName = System.getProperty("browser") != null ? System.getProperty("browser")
+				: prop.getProperty("browser");
+		browserName = prop.getProperty("browser");
 
 		if (browserName.equals("chrome")) {
 
@@ -62,42 +64,41 @@ public class BaseClass {
 	@BeforeMethod(alwaysRun = true)
 	public void launchApplication() throws IOException {
 		driver = initDriver();
-		url=prop.getProperty("url");
+		String url = System.getProperty("url") != null ? System.getProperty("url") : prop.getProperty("url");
 		landingpage = new LandingPage(driver);
 		landingpage.goTo(url);
-		
+
 	}
+
 	@AfterMethod(alwaysRun = true)
-	public void tearDown()
-	{
-	driver.quit();
+	public void tearDown() {
+		driver.quit();
 	}
-	public List<HashMap<String, String>> getJsonDataMap(String filepath) throws IOException
-	{
-		
-		//reading json to String
-		
-		String jsonContent=FileUtils.readFileToString(new File(filepath),StandardCharsets.UTF_16);
-	
-	    // reading String to map Jackson  databind
-		ObjectMapper mapper= new ObjectMapper();
-	   List<HashMap<String, String>> data=	mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
-		});
-		
-	return data;
+
+	public List<HashMap<String, String>> getJsonDataMap(String filepath) throws IOException {
+
+		// reading json to String
+
+		String jsonContent = FileUtils.readFileToString(new File(filepath), StandardCharsets.UTF_16);
+
+		// reading String to map Jackson databind
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+
+		return data;
 	}
-	
-	public String getScreenshot(String testcase,WebDriver driver) throws IOException
-	{
-		TakesScreenshot ts = (TakesScreenshot)driver;
-		 File source=ts.getScreenshotAs(OutputType.FILE);
-		 File file = new File(System.getProperty("user.dir")+"//reports//"+testcase+".png");
-		 FileUtils.copyFile(source, file);
-		 return System.getProperty("user.dir")+"//reports//"+testcase+".png";
+
+	public String getScreenshot(String testcase, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testcase + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "//reports//" + testcase + ".png";
 	}
-	
-	public static Properties getPropertyObject(Properties prop) throws IOException
-	{
+
+	public static Properties getPropertyObject(Properties prop) throws IOException {
 		FileInputStream path = new FileInputStream(System.getProperty("user.dir")
 				+ "/src\\main\\java\\seleniumframework\\Resource\\GlobalData.properties");
 		prop.load(path);
